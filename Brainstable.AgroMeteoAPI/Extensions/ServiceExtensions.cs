0a15878453df62d7b981aa9145b6ceefbc3439ef
Mozgovoy Sergey <brainstable@gmail.com>
2024-02-24 10:50:59 +0300

    Configuration Cors and IISIntegration services

---
+++ Brainstable.AgroMeteoAPI/Extensions/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Brainstable.AgroMeteoAPI.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureQors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("QorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+    }
+}
