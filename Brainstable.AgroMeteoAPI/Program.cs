8c9b31b8abdf3e66277d201993730298aeb8e8a7
Mozgovoy Sergey <brainstable@gmail.com>
2024-03-03 18:47:30 +0300

    Added Swagger

--- Brainstable.AgroMeteoAPI/Program.cs
+++ Brainstable.AgroMeteoAPI/Program.cs
@@ -1,7 +1,8 @@
+using System.Security.Cryptography.Xml;
 using Brainstable.AgroMeteoAPI;
 using Brainstable.AgroMeteoAPI.Extensions;
 using Microsoft.AspNetCore.HttpOverrides;
-
+using Microsoft.OpenApi.Models;
 using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +17,15 @@ builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+builder.Services.AddSwaggerGen(x =>
+{
+    x.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "AgroMeteoAPI",
+        Version = "v1",
+    });
+});
 
 builder.Services.AddControllers(options => options.UseDateOnlyTimeOnlyStringConverters())
     .AddApplicationPart(typeof(Brainstable.AgroMeteoAPI.Presentation.AssemblyReference)
@@ -32,6 +42,11 @@ app.UseExceptionHandler(options => { });
 if (app.Environment.IsProduction())
 {
     app.UseHsts();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "AgroMeteoAPI v1"));
 }
 
 app.UseHttpsRedirection();
